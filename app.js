3482497bec43045b516406623375d954675f5787
Thiago Moura <thiagoguimamoura@hotmail.com>
2024-11-07 20:41:55 -0300

    quebrar lampada

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 /**
- * 
+ * Simples simulador de uma lâmpada
  * @author Thiago Moura
  */
+
+function quebrar(){
+    document.getElementById('lamp').src="img/broken.jpg"
+    //reproduzindo um arquivo de áudio no JS
+    //Passo 1: copiar o arquivo de áudio para o projeto
+    //Passo 2: Usar a classe Audio(biblioteca interna do JS
+    let som = new Audio()
+    som.src = "sound/glassbreaking.wav"
+    som.play()
+}
