01dc75de0103cee3ab8e9626f3d935abcfc1d3f2
Thiago Moura <thiagoguimamoura@hotmail.com>
2024-11-07 22:16:29 -0300

    controle da lampada

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
  * Simples simulador de uma lâmpada
  * @author Thiago Moura
  */
+//variáveis de apoio lógica
+
+let chave = false // o interruptor inica desligado
+let lampada = true //a lâmpada está ok
 
 function quebrar(){
     document.getElementById('lamp').src="img/broken.jpg"
@@ -11,8 +15,26 @@ function quebrar(){
     let som = new Audio()
     som.src = "sound/glassbreaking.wav"
     som.play()
+    // apoio a lógica para o javascript identificar a lâmpada quebrada
+    lampada = false
 }
 
 function onoff(){
+    if (chave === false && lampada === true){
+        // ligar chave
+
     document.getElementById('interruptor').src="img/swon.png"
+    chave = true // O JS agora sabe que a chave está acesa
+
+    //acender a lâmpada
+    document.getElementById('lamp').src="img/on.jpg"
+
+    } else if (lampada === true) {
+    document.getElementById('interruptor').src="img/swoff.png"
+    chave = false
+    
+        //desligar a lâmpada
+    document.getElementById('lamp').src="img/off.jpg"
+    }
+    
 }
